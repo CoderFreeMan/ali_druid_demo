5104efd3084304ee0ad1f073600384f3dd6f3271
yangdj <martonyang0707@163.com>
2019-07-27 15:00:25 +0800

    first submit

---
+++ src/main/java/com/alibaba/druid/demo/DruidDemoApplication.java
@@ -0,0 +1,16 @@
+/**
+ * 启动类
+ */
+package com.alibaba.druid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DruidDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DruidDemoApplication.class, args);
+    }
+
+}
